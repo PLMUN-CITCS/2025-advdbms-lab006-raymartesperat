9c5d838871e1405bc24cc17643270ef5311ae52a
Raymart_xD <esperatraymart_bsit@plmun.edu.com>
2025-02-13 11:59:06 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 04

---
+++ university_db/create_courses_table.sql
@@ -0,0 +1,5 @@
+USE `UniversityDB`;
+CREATE TABLE `Courses` (
+`CourseID` INT PRIMARY KEY AUTO_INCREMENT, 
+`CourseName` VARCHAR(100) NOT NULL
+);
---
+++ university_db/create_enrollments_table.sql
@@ -0,0 +1,9 @@
+USE `UniversityDB`;
+CREATE TABLE `Enrollments` (
+`EnrollmentID` INT PRIMARY KEY AUTO_INCREMENT,
+`StudentID` INT,
+`CourseID` INT,
+`EnrollmentDate` DATE,
+CONSTRAINT `fk_student` FOREIGN KEY (`StudentID`) REFERENCES `Students`(`StudentID`),
+CONSTRAINT `fk_course` FOREIGN KEY (`CourseID`) REFERENCES `Courses`(`CourseID`)
+);
